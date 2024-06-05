dd046dbb3054a9d0c353ae29bd3765022bafe361
Zkine <philippe_jaymes@hotmail.com>
2024-06-05 19:05:23 +0200

    Test backend

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
