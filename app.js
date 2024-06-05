95c722b9e00a48735ce79ede8265f9e34148bff3
Zkine <philippe_jaymes@hotmail.com>
2024-06-05 19:10:33 +0200

    Test backend

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ mongoose
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
