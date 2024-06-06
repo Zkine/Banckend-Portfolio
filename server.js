4d5244f416f7027fda5d26f9913dfe8ec9a5dbed
Zkine <philippe_jaymes@hotmail.com>
2024-06-06 14:30:32 +0200

    Test backend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const normalizePort = (val) => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || 1000);
+const port = normalizePort(1000);
 app.set("port", port);
 
 const errorHandler = (error) => {
