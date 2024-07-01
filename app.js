712893eb23c14f36fb41df00025724ea3a894a8d
Zkine <philippe_jaymes@hotmail.com>
2024-07-01 16:10:41 +0200

    Mise à jour Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -7,25 +7,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
 mongoose
   .connect(`mongodb+srv://${process.env.Key_MongoDB}`)
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers'
-    );
-    next();
-  });
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://portfolio-philippejaymes.fr/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
 
 app.use("/api/message", messageRoute);
 
